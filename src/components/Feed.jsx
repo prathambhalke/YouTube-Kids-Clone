182cd61408dd972073395a712108896b643e9931
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-04 23:45:34 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Kids");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack
+      display="flex"
+      flexDirection="column"
+      // justifyContent="center"
+      sx={{ flexDirection: "column" }}
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        sx={{
+          height: { sx: "auto", md: "100%" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Box>
+
+      <Box
+        p={2}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ overflowY: "auto", height: "90vh", flex: 2 }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo, kidProfile } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack
+    zIndex="2"
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      alignItems: "center",
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <span color="red">
+        <img src={logo} alt="logo" height={65} /> Youtube Kids
+      </span>
+    </Link>
+    <SearchBar />
+    <div
+      style={{ border: "1.5px solid red", borderRadius: "50%", padding: "5px" }}
+    >
+      <img src={kidProfile} alt="kidProfile" height={50} />
+    </div>
+  </Stack>
+);
+
+export default Navbar;
