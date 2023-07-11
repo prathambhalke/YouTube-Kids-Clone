6c1469f988dc587a85cf0c8fd9eb3c283bcfebbc
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-12 00:40:46 +0530

    created a AuthContext for protected routes and optimised the code

--- src/App.js
+++ src/App.js
@@ -11,21 +11,29 @@ import {
   Signup,
   UserProfile,
 } from "./components";
+import { AuthProvider } from "./utils/AuthContext";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
-      <Navbar />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/userprofile" element={<UserProfile />} />
-        <Route exact path="/" element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
-      </Routes>
-    </Box>
+    <AuthProvider>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route
+            exact
+            path="/"
+            element={<ProtectedRoute path="/" element={<Feed />} />}
+          />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </AuthProvider>
   </BrowserRouter>
 );
 
---
+++ src/utils/AuthContext.js
@@ -0,0 +1,60 @@
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+
+  const login = (e, email, password) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage);
+        console.log(errorCode, errorMessage);
+      });
+    setIsAuthenticated(true);
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      alert("signedOut successfully");
+      setIsAuthenticated(false);
+      navigate("/login");
+    } catch {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
