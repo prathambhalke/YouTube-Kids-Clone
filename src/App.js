531b1ff6a58aab6e6b6e166f4936f42631ded4f5
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-12 23:58:05 +0530

    added the theme mode functionality button and it also changes accordingly to user system

--- src/App.js
+++ src/App.js
@@ -15,36 +15,29 @@ import { AuthProvider } from "./utils/AuthContext";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import { backgroundImg } from "./utils/constants";
 
-const App = () => (
-  <BrowserRouter>
-    <AuthProvider>
-      <Box
-        sx={{
-          background: `url(${backgroundImg})`,
-          height: "100%",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "repeat-y",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        <Navbar />
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/userprofile" element={<UserProfile />} />
-          <Route
-            exact
-            path="/"
-            element={<ProtectedRoute path="/" element={<Feed />} />}
-          />
-          <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </AuthProvider>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Box>
+          <Navbar />
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/userprofile" element={<UserProfile />} />
+            <Route
+              exact
+              path="/"
+              element={<ProtectedRoute path="/" element={<Feed />} />}
+            />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
+import ThemeProviderWrapper from "./utils/ThemeProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <ThemeProviderWrapper>
+    <App />
+  </ThemeProviderWrapper>
+);
