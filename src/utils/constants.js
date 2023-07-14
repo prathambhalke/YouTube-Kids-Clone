ed147230222b9517714d49fdff15c27cd19e72f9
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-14 23:25:22 +0530

    Sanitized the code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,53 +1,22 @@
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import HomeIcon from "@mui/icons-material/Home";
-import CodeIcon from "@mui/icons-material/Code";
-import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import LiveTvIcon from "@mui/icons-material/LiveTv";
-import SchoolIcon from "@mui/icons-material/School";
-import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
-import CheckroomIcon from "@mui/icons-material/Checkroom";
-import GraphicEqIcon from "@mui/icons-material/GraphicEq";
-import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
-import Logo from "../utils/logo1.png";
-import kid from "./kid.png";
-import loginShichan from "./shinc.jpg";
-import background from "./background.jpg";
-import popcorn from "./popcorn.png";
-import music from "./music1.png";
-import bulb from "./bulb.png";
-import funny from "./funny.png";
+import Logo from "../Assets/logo1.png";
+import kid from "../Assets/kid.png";
+import loginShichan from "../Assets/shinc.jpg";
+import background from "../Assets/background.jpg";
+import popcorn from "../Assets/popcorn.png";
+import music from "../Assets/music1.png";
+import bulb from "../Assets/bulb.png";
+import funny from "../Assets/funny.png";
 
-// export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 export const logo = Logo;
 export const kidProfile = kid;
 export const loginShin = loginShichan;
 export const backgroundImg = background;
-// export const popcornImg = popcorn;
-// export const musicImg = music;
-// export const bulbImg = bulb;
-// export const funnyImg = funny;
 
 export const categories = [
   { name: "Kids", icon: popcorn },
   { name: "Kids Music", icon: music },
   { name: "Kids Learing", icon: bulb },
   { name: "Kids Explore", icon: funny },
-  // { name: "NextJS", icon: <CodeIcon /> },
-  // { name: "Music", icon: <MusicNoteIcon /> },
-  // { name: "Education", icon: <SchoolIcon /> },
-  // { name: "Podcast", icon: <GraphicEqIcon /> },
-  // { name: "Movie", icon: <OndemandVideoIcon /> },
-  // { name: "Gaming", icon: <SportsEsportsIcon /> },
-  // { name: "Live", icon: <LiveTvIcon /> },
-  // { name: "Sport", icon: <FitnessCenterIcon /> },
-  // { name: "Fashion", icon: <CheckroomIcon /> },
-  // { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
-  // { name: "Comedy", icon: <TheaterComedyIcon /> },
-  // { name: "Gym", icon: <FitnessCenterIcon /> },
-  // { name: "Crypto", icon: <DeveloperModeIcon /> },
 ];
 
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
