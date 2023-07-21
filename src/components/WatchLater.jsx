f1459aeb02fb5e5ade4082ed7638898e74c08d3a
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-21 22:46:07 +0530

    changes in historypage, likedvideospage and watch later page

--- src/components/WatchLater.jsx
+++ src/components/WatchLater.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 const WatchLater = () => {
   return (
-    <div>WatchLater</div>
-  )
-}
+    <Grid container style={{ background: "#000", height: "100vh" }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "blue" }}>
+        Watch Later {"  "}
+        <span style={{ color: "#FC1503" }}>videos</span>
+      </Typography>
+    </Grid>
+  );
+};
 
-export default WatchLater
+export default WatchLater;
