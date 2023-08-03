667c6f9660badf889b20b3b9f3906ef6513f67ac
prathambhalke <prathameshbhalke125@gmail.com>
2023-08-03 23:43:15 +0530

    changes

--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -2,11 +2,9 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { watchLaterSlice } from "./../Features/watchLaterSlice";
 import { likedVideosSlice } from "./../Features/likedVideosSlice";
 import { watchHistorySlice } from "../Features/watchHistorySlice";
-import videosSlice from "../Features/videosSlice";
 
 export const rootReducer = combineReducers({
   watchLaterSlice: watchLaterSlice.reducer,
   LikedVideosSlice: likedVideosSlice.reducer,
   watchHistorySlice: watchHistorySlice.reducer,
-  videosSlice: videosSlice.reducer,
 });
